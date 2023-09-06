5b10b3a0c4e22b97ff02dfc596d4b351a1b06f61
pxlly <apollo.pierce101@gmail.com>
2023-09-06 19:08:59 -0400

    adding it to github

---
+++ git-lab-program.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int arg, char const *argv[]) {
+
+    cout << "Hello Git!!";
+
+
+
+    return 0;
+}
+
+/// Apollo Pierce 9/6/23
